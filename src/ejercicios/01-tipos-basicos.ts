c5d329b29bf6f2b1059702cadb74c108c78522d7
NoMati <mati.oyarzo@duocuc.cl>
2023-01-13 19:41:07 -0300

    vivo

--- src/ejercicios/01-tipos-basicos.ts
+++ src/ejercicios/01-tipos-basicos.ts
@@ -4,9 +4,9 @@
 
 let nombre: string = 'Strider';
 let hp: number | string = 95;
-let estaVivo: boolean = false;
+let vivo: boolean = false;
 
 hp = 'FULL';
 
-console.log(nombre, hp, estaVivo);
+console.log(nombre, hp, vivo);
 
